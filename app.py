20669e042aad223d02f71693e029cd2c1d4a1e4d
edson alves <erbjturbo4@gmail.com>
2025-01-03 02:10:32 -0300

    Adicionar rota raiz e verificar configuração do Procfile

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def home():
+    return "Welcome to the API!"
+
 @app.route('/api/player', methods=['GET'])
 def get_player_data():
     try:
